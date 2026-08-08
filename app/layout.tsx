25469a77aa81da6130a6c7447ac37fae31c4394b
John Dev <kingsoorsa@gmail.com>
2026-08-08 17:42:17 +0100

    chore::update FB pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { montserrat } from "@/app/fonts";
 import Navbar from "@/components/layout/NavBar";
 import Modal from "@/components/ui/Modal";
-import * as FBpixel from "@/utils/facebook.pixel";
 import type { Metadata } from "next";
 import Script from "next/script";
 import "./globals.css";
@@ -24,16 +23,16 @@ export default function RootLayout({
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://facebook.net/en_US/fbevents.js');
-            fbq('init', '${FBpixel.FB_PIXEL_ID}');
-            fbq('track', 'PageView');
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1043029898623521');
+fbq('track', 'PageView');
           `,
           }}
         />
